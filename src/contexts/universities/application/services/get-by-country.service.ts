3fd98f56f595f4c17bd33650ed5d78fb8b13b9ac
angeldeathz <martin.david.dev@gmail.com>
2024-01-10 23:20:10 -0300

    feat: added some logs and updated regex library for github actions

--- src/contexts/universities/application/services/get-by-country.service.ts
+++ src/contexts/universities/application/services/get-by-country.service.ts
@@ -1,10 +1,12 @@
 import { get } from "../../../../infrastructure/http/http-client";
+import { logger } from "../../../../infrastructure/log/logger";
 import { UNIVERSITIES_API } from "../../../../infrastructure/utils/config";
 import { University } from "../../domain/university";
 
 const getByCountry = async (country: string) => {
   const url = `${UNIVERSITIES_API}/search?country=${country}`;
   const { data: universities } = await get<University[]>(url);
+  logger.info(`Total ${universities.length} universities in ${country}`);
   return universities;
 };
 
